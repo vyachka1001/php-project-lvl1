b20352f87be42d92789724f7556925da7de67553
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-01-27 13:21:00 +0300

    add username returning

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,10 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greetUser()
+function getUserName(): string
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?', false, ' ', false);
     line("Hello, %s!", $name);
+    return $name;
 }
