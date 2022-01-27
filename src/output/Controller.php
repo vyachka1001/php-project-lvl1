df7f3c5e1899a8218ac78305c55b50f9a943b12a
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-01-27 13:19:35 +0300

    add get/write answer and endGame functions

--- src/output/Controller.php
+++ src/output/Controller.php
@@ -3,9 +3,14 @@
 namespace Brain\Games\Src\Output\Controller;
 
 use function cli\line;
+use function cli\prompt;
 
 const EVEN_GAME_CONDITION = 'Answer "yes" if the number is even, otherwise answer "no".';
 const QUESTION_STR = "Question: ";
+const ANSWER_STR = "Your answer";
+const CORRECT_ANSWER_OUTPUT = 'Correct!';
+const VICTORY_END_STR = "Congratulations, ";
+const DEFEAT_END_STR = "Let's try again, ";
 
 function writeCondition($game)
 {
@@ -18,4 +23,28 @@ function writeCondition($game)
 function writeQuestion($question)
 {
     line(QUESTION_STR . $question);
+}
+
+function getAnswer(): string
+{
+    $answer = prompt(ANSWER_STR);
+    return $answer;
+}
+
+function writeAnswer($isCorrect, $correctAnswer = '', $userAnswer = '')
+{
+    if ($isCorrect) {
+        line(CORRECT_ANSWER_OUTPUT);
+    } else {
+        line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'");
+    }
+}
+
+function endGame($isVictory, $name)
+{
+    if ($isVictory) {
+        line(VICTORY_END_STR . $name . "!");
+    } else {
+        line(DEFEAT_END_STR . $name . "!");
+    }
 }
