f30283606dae209729fc978e9c137786e31b08a5
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-01-27 12:14:48 +0300

    add writeQuestion impl

--- src/output/Controller.php
+++ src/output/Controller.php
@@ -5,6 +5,7 @@ namespace Brain\Games\Output\Controller;
 use function cli\line;
 
 const EVEN_GAME_CONDITION = 'Answer "yes" if the number is even, otherwise answer "no".';
+const QUESTION_STR = "Question: ";
 
 function writeCondition($game)
 {
@@ -12,4 +13,9 @@ function writeCondition($game)
         case 'even':
             line(EVEN_GAME_CONDITION);
     }
+}
+
+function writeQuestion($question)
+{
+    line(QUESTION_STR . $question);
 }
