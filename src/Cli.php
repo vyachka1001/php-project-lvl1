fb73b3b99dfb1175424a75c84ff345a409d5f468
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-01-28 23:42:21 +0300

    fix prompt params

--- src/Cli.php
+++ src/Cli.php
@@ -8,7 +8,7 @@ use function cli\prompt;
 function getUserName(): string
 {
     line('Welcome to the Brain Games!');
-    $name = prompt('May I have your name?', false, ' ', false);
+    $name = prompt('May I have your name?', '', ' ', false);
     line("Hello, %s!", $name);
     return $name;
 }
