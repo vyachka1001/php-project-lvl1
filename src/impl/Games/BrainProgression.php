b8336e1a9f03da154942defeaf8b123f9aa420a3
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-01-28 19:35:35 +0300

    add generate and count answer function

---
+++ src/impl/Games/BrainProgression.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Src\Impl\Games\BrainProgression;
+
+use function Src\Impl\Generators\getRandomInt;
+
+const PASS_MARKER = "..";
+const MIN_INDEX = 1;
+const MAX_INDEX = 9;
+
+function generate(): string
+{
+    $firstEl = getRandomInt();
+    $diff = getRandomInt();
+    $passIndex = getRandomInt(MIN_INDEX, MAX_INDEX);
+    $progression = [$firstEl];
+
+    for ($index = MIN_INDEX; $index < MAX_INDEX + 1; $index++) {
+        if ($index === $passIndex) {
+            $progression[] = PASS_MARKER;
+        } else {
+            $progression[] = $firstEl + $index * $diff;
+        }
+    }
+
+    return implode(' ', $progression);
+}
+
+function countAnswer($expression): string
+{
+    $progression = explode(' ', $expression);
+    $firstEl = $progression[0];
+    $diff = 0;
+    $passIndex = 0;
+    $index = 1;
+
+    while ($diff === 0 || $passIndex === 0) {
+        if ($progression[$index] !== PASS_MARKER && $progression[$index - 1] !== PASS_MARKER) {
+            $diff = $progression[$index] - $progression[$index - 1];
+        }
+
+        if ($progression[$index] === PASS_MARKER) {
+            $passIndex = $index;
+        }
+
+        $index++;
+    }
+
+    return $firstEl + $passIndex * $diff;
+}
