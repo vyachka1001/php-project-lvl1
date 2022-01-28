fbfb4be3eceebe08a9a00515b934edddd95315a6
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-01-28 11:14:41 +0300

    left only generation and count answer functions

--- src/impl/BrainEven.php
+++ src/impl/BrainEven.php
@@ -2,38 +2,14 @@
 
 namespace Src\Impl\BrainEven;
 
-use function Brain\Games\Src\Output\Controller\writeQuestion;
 use function Src\Impl\Generators\getRandomInt;
-use function Brain\Games\Src\Output\Controller\getAnswer;
-use function Brain\Games\Src\Output\Controller\writeAnswer;
-use function Brain\Games\Src\Output\Controller\endGame;
 
-function playRound(): bool
+function generate(): int
 {
-    $value = getRandomInt();
-    writeQuestion($value);
-
-    $answer = ($value % 2 === 0) ? 'yes' : 'no';
-    $userAnswer = getAnswer();
-
-    if ($answer === $userAnswer) {
-        writeAnswer(true);
-        return true;
-    } else {
-        writeAnswer(false, $answer, $userAnswer);
-        return false;
-    }
+    return getRandomInt();
 }
 
-function play($name)
+function countAnswer($expression)
 {
-    for ($round = 0; $round < 3; $round++) {
-        $roundResult = playRound();
-        if (!$roundResult) {
-            endGame(false, $name);
-            return;
-        }
-    }
-
-    endGame(true, $name);
+    return ($expression % 2 === 0) ? 'yes' : 'no';
 }
