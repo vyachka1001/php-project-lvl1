94f54469700a0bf6531bb7d73c253d2894d201ac
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-01-28 23:55:41 +0300

    fix function param

--- src/impl/generators/RandomInt.php
+++ src/impl/generators/RandomInt.php
@@ -5,7 +5,7 @@ namespace Src\Impl\Generators;
 const RAND_INT_MIN = 1;
 const RAND_INT_MAX = 100;
 
-function getRandomInt($min = RAND_INT_MIN, $max = RAND_INT_MAX)
+function getRandomInt(int $min = RAND_INT_MIN, int $max = RAND_INT_MAX)
 {
     return random_int($min, $max);
 }
