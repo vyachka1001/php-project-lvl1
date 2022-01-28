be59059b4f775d11b950f25a003a0663bc926ac2
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-01-28 11:57:24 +0300

    create generate and count answer functions

---
+++ src/impl/BrainCalc.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Src\Impl\BrainCalc;
+
+use function Src\Impl\Generators\getRandomInt;
+
+function generate(): string
+{
+    $operationSigns = ['+', '-', '*'];
+    $sign = $operationSigns[getRandomInt(0, count($operationSigns) - 1)];
+    $operandOne = getRandomInt();
+    $operandTwo = getRandomInt();
+
+    return "{$operandOne} {$sign} {$operandTwo}";
+}
+
+function countAnswer($expression): string
+{
+    $values = explode(' ', $expression);
+    $signIndex = 1;
+    $operandOne = $values[0];
+    $operandTwo = $values[2];
+
+    switch ($values[$signIndex]) {
+        case '+':
+            return $operandOne + $operandTwo;
+        case '-':
+            return $operandOne - $operandTwo;
+        case '*':
+            return $operandOne * $operandTwo;
+    }
+}
